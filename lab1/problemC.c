f0dc3c423642221f47a0af9c80548ddfa4fc27c4
Elizabeth D. Wilson 22 <ediewilson@flume.cs.dartmouth.edu>
2020-04-04 14:47:40 -0400

    final commit

--- lab1/problemC.c
+++ lab1/problemC.c
@@ -13,7 +13,7 @@ int main (void) {
 
     int sockfd, sockServ; 
     struct sockaddr_in serverAddress, clientAddress;  
-    char *message = "HTTP/1.1 200 OK\n\nEdie\n"; 
+    char *message = "Edie"; 
 
     if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         printf("error opening socket");
@@ -23,11 +23,11 @@ int main (void) {
     memset(&clientAddress, 0, sizeof(clientAddress));
 
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY; 
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
     serverAddress.sin_port = htons(PORT); 
 
     clientAddress.sin_family = AF_INET;
-    clientAddress.sin_addr.s_addr = INADDR_ANY;
+    clientAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
     clientAddress.sin_port = htons(PORT);
 
     if (bind(sockfd,(struct sockaddr *)&serverAddress,sizeof(serverAddress)) < 0) { 
@@ -35,7 +35,9 @@ int main (void) {
         exit(1); 
     } else printf("binding success\n");
 
-    sendto(sockfd, (const char *)message, strlen(message),0, (const struct sockaddr *) &clientAddress, sizeof(clientAddress)); 
+    unsigned int length = sizeof(clientAddress);
+
+    sendto(sockfd, (const char *)message, strlen(message),0, (const struct sockaddr *) &clientAddress,length); 
     
     printf("Hello message sent.\n");
 
